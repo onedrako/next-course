49c7948ad3465160ebfc97ea5ca4fbb660a62333
onedrako <onedrako@gmail.com>
2022-02-24 13:21:12 -0600

    retry with fetch in relative route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import fetch from 'isomorphic-unfetch'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-export const getStaticProps = async () => {
-    const response = await fetch('http://localhost:3000/api/avo')
-    const {data: productList}: TAPIAvoResponse  = await response.json()
+const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
 
-  return {
-    props: {
-      productList,
-    }
-  }
-}
-
-const HomePage = ({productList}: {productList: TProduct[]}) => {
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({ data }: TAPIAvoResponse) => {
+        setProductList(data)
+      })
+  }, [])
 
   return (
     <Layout>
